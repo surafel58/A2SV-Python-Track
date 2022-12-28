23fa8fe79d63e2b9016872cdab17ee460d9c4edb
surafel <72102647+surafel58@users.noreply.github.com>
2022-12-28 17:29:36 +0300

    Solution for set difference operation problem from hackerrank

---
+++ Set difference operation.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+english_subscr_size = int(input())
+english_subscription = set(input().split())
+french_subscr_size = int(input())
+french_subscription = set(input().split())
+
+english_subscr_only = english_subscription.difference(french_subscription)
+
+print(len(english_subscr_only))
