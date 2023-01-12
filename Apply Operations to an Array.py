8cd3f128ee7ba93d064d654b756fcdfc7603432e
surafel <72102647+surafel58@users.noreply.github.com>
2023-01-12 19:37:21 +0300

    Solution for Apply Operations to an Array problem from leetcode

---
+++ Apply Operations to an Array.py
@@ -0,0 +1,23 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        #[1,0,2,0,0,1]
+
+        for index in range(len(nums) - 1):
+            if nums[index] == nums[index+1]:
+                nums[index] = nums[index] * 2
+                nums[index + 1] = 0
+
+        write = 0
+        read = 0
+
+        while read < len(nums):
+            if nums[read] != 0:
+                temp = nums[read]
+                nums[read] = nums[write]
+                nums[write] = temp
+            
+                write =  write + 1
+            
+            read = read + 1
+
+        return nums
